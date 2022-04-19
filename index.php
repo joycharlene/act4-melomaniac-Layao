2489f1b6b504f9794fe42aa53081d62d88905599
LayaoCharleneJoy <layaocharlenejoy@gmail.com>
2022-04-19 13:14:59 +0700

    final index

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<html>
+<body style="background-color: gray;">
 <html lang="en">
     <head>
         <meta charset="utf-8" />
@@ -12,13 +14,13 @@
     </head>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-warning fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="#page-top"> Exprimere Te </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 1.php">Song 1</a></li>
                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
@@ -28,28 +30,156 @@
             </div>
         </nav>
         <!-- Header-->
-        <header class="bg-dark bg-gradient text-white">
+        <header class="bg-dark bg-gradient text-warning">
             <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
+                <h1 class="fw-bolder">Welcome to Exprimere Te</h1>
+                <p class="lead"> Express what you feel, let go your burdens, chill your mind, sing your lungs out!</p>
                 
             </div>
         </header>
-        <!-- About section-->
+        <!--About section-->
         <section id="songs">
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-primary">Song Title here</h2>
-                        <p class="lead">display the Composer here</p>
-                        <p>display the lyrics/chords here</p>
+                        
+                        <?php 
+                        echo "<h2 class='text-warning'>Wait A Minute</h2>";
+                        echo "<p class='lead '>Willow Smith</p>";
+                
+                        echo "<p> [Instrumental Intro]</p>";
+                        echo "<h5><pre> D# B C# D#m G#m B A#m D#m G#m</h5>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>B         </h5>";
+                        echo "<p>Wait a minute!</p>";
+                        echo "<h5><pre>  C#                 D#m                </h5>";
+                        echo "<p>I think I left my conscience on your front door step</p>";
+                        echo "<h5><pre>B         </h5>";
+                        echo "<p>Wait a minute!</p>"; 
+                        echo "<h5><pre>      A#m                D#m                B    </h5>";
+                        echo "<p>I think I left my consciousness in the 6th dimension</p>";                
+                        echo "<br/>";
+
+                        echo "<h5><pre>                         A#m</h5>";
+                        echo "<p>But I'm here right now, right now</p>";
+                        echo "<p>Just sitting in a cloud, oh, wow</p>";
+                        echo "<h5><pre>D#m                  </h5>";
+                        echo "<p>I'm here right now, right now</p>";
+                        echo "<h5><pre>G#m                B</h5>";
+                        echo "<p>With you, oh wow, oh wow</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>              A#m</h5>";
+                        echo "<p>I don't even care</p>";
+                        echo "<h5><pre>                            D#m</h5>";
+                        echo "<p>I'll run my hands through your hair</p>";
+                        echo "<p>You wanna run your fingers through mine</p>";
+                        echo "<h5><pre>    G#m                            B   </h5>";
+                        echo "<p> But my dreads too thick and that's alright</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>                A#m</h5>";
+                        echo "<p>Hold on, wait a minute!</p>";
+                        echo "<h5><pre>                 D#m</h5>";
+                        echo "<p>Feel my heart's intention</p>";
+                        echo "<h5><pre>                G#m</h5>";
+                        echo "<p>Hold on, wait a minute!</p>";
+                        echo "<h5><pre>                    B                </h5>";
+                        echo "<p>I left my consciousness in the 6th dimension</p>";
+                        echo "<h5><pre>A#m      </h5>";
+                        echo "<p>Left my soul in his vision</p>";
+                        echo "<h5><pre>  D#m               </h5>";
+                        echo "<p>Let's go get it, oh, oh</p>";
+                        echo "<h5><pre>  G#m              B</h5>";
+                        echo "<p>Let's go get it, oh, oh</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>                 C# </h5>";
+                        echo "<p>Some things don't work</p>";
+                        echo "<h5><pre>            D#m    </h5>";
+                        echo "<p>Some things are bound to be</p>";
+                        echo "<h5><pre>                B</h5>";
+                        echo "Some things—they hurt</p>";
+                        echo "<h5><pre>          D#m   </h5>";
+                        echo "<p>And they tear apart me</p>";   
+                        echo "<h5><pre>               C#        </h5>";
+                        echo "<p>You left your diary at my house</p>";
+                        echo "<h5><pre>  D#m              </h5>";
+                        echo "<p>And I read those pages.</p>";
+                        echo "<h5><pre>B                    </h5>";
+                        echo "<p>Do you really love me, baby?</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>                  A#m</h5>";
+                        echo "<p>Some things don't work</p>";
+                        echo "<h5><pre>            D#m     </h5>";
+                        echo "<p>Some things are bound to be</p>";
+                        echo "<h5><pre>                 G#m</h5>";
+                        echo "<p>Some things—they hurt</p>";
+                        echo "<h5><pre>              B  </h5>";
+                        echo "<p>And they tear apart me</p>";
+                        echo "<h5><pre>              A#m </h5>";
+                        echo "<p>But I broke my word</p>";
+                        echo "<h5><pre>      D#m       </h5>";
+                        echo "<p>And you were bound to see</p>";
+                        echo "<h5><pre>           G#m   </h5>";
+                        echo "<p>And I cried at the curb</p>";
+                        echo "<h5><pre>                             B   </h5>";
+                        echo "<p>When you first said, Oel ngati kameie. </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>                    A#m </h5>";
+                        echo "<p>Hold on, wait a minute!</p>";
+                        echo "<h5><pre>                  D#m</h5>";
+                        echo "<p>Feel my heart's intention</p>";
+                        echo "<h5><pre>                    G#m </h5>";
+                        echo "<p>Hold on, wait a minute!</p>";
+                        echo "<h5><pre>                     B                </h5>";
+                        echo "<p>I left my consciousness in the 6th dimension</p>";
+                        echo "<h5><pre>A#m      </h5>";
+                        echo "<p>Left my soul in his vision</p>";
+                        echo "<h5><pre>  D#m               </h5>";
+                        echo "<p>Let's go get it, oh, oh</p>";
+                        echo "<h5><pre>  G#m              B</h5>";
+                        echo "<p>Let's go get it, oh, oh</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>        C#m   </h5>";
+                        echo "<p>Some people like to live</p>";
+                        echo "<h5><pre>               G#m   </h5>";
+                        echo "<p>Some just tryin' to get by</p>";
+                        echo "<h5><pre>         C#m      </h5>";
+                        echo "<p>Some people like that hurt</p>";
+                        echo "<h5><pre>                  G#m       B</h5>";
+                        echo "<p>Some just rather say goodbye, bye</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>                     A#m </h5>";
+                        echo "<p>Hold on, wait a minute!</p>";
+                        echo "<h5><pre>                   D#m </h5>";
+                        echo "<p>Feel my heart's intention</p>";
+                        echo "<h5><pre>                     G#m </h5>";
+                        echo "<p>Hold on, wait a minute!</p>";
+                        echo "<h5><pre>                    B                </h5>";
+                        echo "<p>I left my consciousness in the 6th dimension</p>";
+                        echo "<h5><pre> A#m         </h5>";
+                        echo "<p>Left my soul in his vision</p>";
+                        echo "<h5><pre>  D#m               </h5>";
+                        echo "<p>Let's go get it, oh, oh</p>";
+                        echo "<h5><pre>  G#m              B</h5>";
+                        echo "<p>Let's go get it, oh, oh</p>";
+                        echo "<br/>";
+
+                        ?>
+
                     </div>
                 </div>
             </div>
         </section>
 
         <footer class="py-3 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add your fullname here</p></div>
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Charlene Joy Layao</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
@@ -57,3 +187,4 @@
         <script src="js/scripts.js"></script>
     </body>
 </html>
+           
