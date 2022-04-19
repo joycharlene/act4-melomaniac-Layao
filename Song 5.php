18ecf83b9149d9049e554965cd01f8a718583f6a
LayaoCharleneJoy <layaocharlenejoy@gmail.com>
2022-04-19 18:47:53 +0700

    Song 5 final

---
+++ Song 5.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:gray;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-warning fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Exprimetere Te </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-warning">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Exprimere Te</h1>
+                <p class="lead">Express what you feel, let go your burdens, chill your mind, sing your lungs out!</p> 
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+                        echo "<h2 class='text-warning'>Sanctuary</h2>";
+                        echo "<p class='lead '>Joji</p>";
+
+                        echo "<p> [Instrumental Intro]</p>";
+                        echo "<h5><pre>G  Cmaj7  G</h5>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>G                            </h5>";
+                        echo "<p> Go ahead and bark after dark, fallen star </p>";
+                        echo "<h5><pre>           Cmaj7   </h5>";
+                        echo "<p> I'm your one-call away </p>";
+                        echo "<h5><pre>G               </h5>";   
+                        echo "<p> Motel halls, neon walls </p>";
+                        echo "<h5><pre>             Cmaj7  </h5>";
+                        echo "<p> When night falls I am your escape </p>";
+                        echo "<h5><pre>Bm             Am   </h5>";   
+                        echo "<p> When you lay alone, I ache </p>";
+                        echo "<h5><pre>F                   </h5>";
+                        echo "<p> Something I wanted to feel  </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>G                 Cmaj7     </h5>";
+                        echo "<p> If you've been waiting for falling in love </p>";
+                        echo "<h5><pre>            Am                 F  </h5>";
+                        echo "<p> Babe, you don't have to wait on me </p>";
+                        echo "<h5><pre>G                     Cmaj7      </h5>";
+                        echo "<p> 'Cause I've been aiming for heaven above </p>";
+                        echo "<h5><pre>         Am                F </h5>";
+                        echo "<p> But an angel ain't what I need </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>  G                      </h5>";
+                        echo "<p> Not anyone, you're the one </p>";
+                        echo "<h5><pre>                   Cmaj7 </h5>";
+                        echo "<p> More than fun, you're the sanctuary </p>";
+                        echo "<h5><pre> G                              Cmaj7</h5>";
+                        echo "<p> 'Cause what you want is what I want, sincerity </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>Bm                Am      </h5>";
+                        echo "<p> Souls that dream alone lie awake </p>";
+                        echo "<h5><pre>F                      </h5>";
+                        echo "<p> I'll give you something so real </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>G                 Cmaj7     </h5>";
+                        echo "<p> If you've been waiting for falling in love </p>";
+                        echo "<h5><pre>            Am                 F  </h5>";
+                        echo "<p> Babe, you don't have to wait on me </p>";
+                        echo "<h5><pre>G                     Cmaj7      </h5>";
+                        echo "<p> 'Cause I've been aiming for heaven above </p>";
+                        echo "<h5><pre>         Am                F </h5>";
+                        echo "<p> But an angel ain't what I need </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>  F                Em </h5>";
+                        echo "<p> Hold me oh so close, 'cause you never know </p>";    
+                        echo "<h5><pre>  D         </h5>";  
+                        echo "<p> Just how long our lives will be </p>";    
+                        echo "<br/>";
+
+                        echo "<h5><pre>G                 Cmaj7     </h5>";
+                        echo "<p> If you've been waiting for falling in love </p>";
+                        echo "<h5><pre>            Am                 F  </h5>";
+                        echo "<p> Babe, you don't have to wait on me </p>";
+                        echo "<h5><pre>G                     Cmaj7      </h5>";
+                        echo "<p> 'Cause I've been aiming for heaven above </p>";
+                        echo "<h5><pre>         Am                F </h5>";
+                        echo "<p> But an angel ain't what I need </p>";
+                        echo "<h5><pre>C    Am     G </h5>";
+                        echo "<br/>";
+
+                        
+                        echo "-----------------------------------------";
+                        echo "<br/>";
+
+                        ?>
+                        Back to main page, Click <a href = "index.php"> here!
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Charlene Joy Layao</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
