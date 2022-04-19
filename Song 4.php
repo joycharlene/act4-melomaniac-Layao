06276e87d5627165fd648cdd00cd89443a44a393
LayaoCharleneJoy <layaocharlenejoy@gmail.com>
2022-04-19 18:09:04 +0700

    Song 4 final

---
+++ Song 4.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:gray;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-warning fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Exprimetere Te </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-warning">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Exprimere Te</h1>
+                <p class="lead">Express what you feel, let go your burdens, chill your mind, sing your lungs out!</p> 
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+                        echo "<h2 class='text-warning'>Play Date</h2>";
+                        echo "<p class='lead '>Melanie Marquez</p>";
+
+                        echo "<p> [Instrumental Intro]</p>";
+                        echo "<h5><pre>Gm-C-Am-Dm-Gm-C-F (2x)</h5>";
+                        echo "<br/>";
+                                 
+                        echo "<h5><pre>   Gm          C         Am      Dm  </h5>";
+                        echo "<p> You call me on the telephone, you feel so far away </p>";
+                        echo "<h5><pre>   Gm          C             F               </h5>";
+                        echo "<p> You tell me to come over, there's some games you wanna play </p>";
+                        echo "<h5><pre>    Gm          C    Am     Dm  </h5>";   
+                        echo "<p> I'm walking to your house, nobody's home </p>";
+                        echo "<h5><pre>     Gm           C        F    </h5>";
+                        echo "<p> Just me and you and you and me alone </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>Gm      C            Am  </h5>";
+                        echo "<p> We're just playing hide and seek </p>";
+                        echo "<h5><pre>     Dm        Gm     C               F  </h5>";
+                        echo "<p> It's getting hard to breathe under the sheets with you </p>";
+                        echo "<h5><pre>Gm    C           Am   </h5>";
+                        echo "<p> I don't want to play no games </p>";
+                        echo "<h5><pre>    Dm         Gm     C         F  </h5>";
+                        echo "<p> I'm tired of always chasing, chasing after you </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>Gm        C           Am Dm </h5>";
+                        echo "<p> I don't give a fuck about you anyways </p>";
+                        echo "<h5><pre>Gm       C          F</h5>";
+                        echo "<p> Whoever said I gave a shit 'bout you? </p>";
+                        echo "<h5><pre>    Gm          C        Am Dm </h5>";
+                        echo "<p> You never share your toys or communicate </p>";
+                        echo "<h5><pre>Gm          C        F</h5>";
+                        echo "<p> I guess I'm just a play date to you </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>Gm          C               Am         Dm</h5>";
+                        echo "<p> Wake up in your bedroom and there's nothing left to say </p>";    
+                        echo "<h5><pre>G         C            F              </h5>";  
+                        echo "<p> When I try to talk you're always playing board games </p>";
+                        echo "<h5><pre> Gm          C     Am     Dm   </h5>";
+                        echo "<p> I wish I had monopoly over your mind </p>";    
+                        echo "<h5><pre> Gm       C       F  </h5>";
+                        echo "<p> I wish I didn't care all the time </p>";  
+                        echo "<br/>";
+
+                        echo "<h5><pre>Gm      C            Am  </h5>";
+                        echo "<p> We're just playing hide and seek </p>";
+                        echo "<h5><pre>     Dm        Gm     C               F  </h5>";
+                        echo "<p> It's getting hard to breathe under the sheets with you </p>";
+                        echo "<h5><pre>Gm    C           Am   </h5>";
+                        echo "<p> I don't want to play no games </p>";
+                        echo "<h5><pre>    Dm         Gm     C         F  </h5>";
+                        echo "<p> I'm tired of always chasing, chasing after you </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>Gm        C           Am Dm </h5>";
+                        echo "<p> I don't give a fuck about you anyways </p>";
+                        echo "<h5><pre>Gm       C          F</h5>";
+                        echo "<p> Whoever said I gave a shit 'bout you? </p>";
+                        echo "<h5><pre>    Gm          C        Am Dm </h5>";
+                        echo "<p> You never share your toys or communicate </p>";
+                        echo "<h5><pre>Gm          C        F</h5>";
+                        echo "<p> I guess I'm just a play date to you </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>Gm          C     </h5>";
+                        echo "<p> Ring around the rosy </p>";
+                        echo "<h5><pre>Am          Dm                   </h5>";
+                        echo "<p> I never know, I never know what you need </p>";
+                        echo "<h5><pre>Gm          C              Am             </h5>";
+                        echo "<p> Ring around the rosy, I wanna give you, wanna give you </p>";
+                        echo "<h5><pre>Dm              </h5>";
+                        echo "<p> What you need </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>Gm        C           Am Dm </h5>";
+                        echo "<p> I don't give a fuck about you anyways </p>";
+                        echo "<h5><pre>Gm       C          F</h5>";
+                        echo "<p> Whoever said I gave a shit 'bout you? </p>";
+                        echo "<h5><pre>    Gm          C        Am Dm </h5>";
+                        echo "<p> You never share your toys or communicate </p>";
+                        echo "<h5><pre>Gm          C        F</h5>";
+                        echo "<p> I guess I'm just a play date to you </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre> Gm         C           Am Dm</h5>";
+                        echo "<p> You know I give a fuck about you everyday </p>";
+                        echo "<h5><pre>Gm            C         F  </h5>";
+                        echo "<p> Guess it's time that I tell you the truth </p>";
+                        echo "<h5><pre>Gm        C         Am   Dm  </h5>";
+                        echo "<p> If I share my toys, will you let me stay? </p>";
+                        echo "<h5><pre>  Gm           C          F</h5>";
+                        echo "<p> Don't wanna leave this play date with you </p>";
+                        echo "<br/>";
+
+
+                        echo "-----------------------------------------";
+                        echo "<br/>";
+
+                        ?>
+                        Back to main page, Click <a href = "index.php"> here!
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Charlene Joy Layao</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
