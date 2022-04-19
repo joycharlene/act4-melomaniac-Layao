b4baeba47da438a682d1db7aed1ca3d9de93e83e
LayaoCharleneJoy <layaocharlenejoy@gmail.com>
2022-04-19 15:42:51 +0700

    Song 2 final

---
+++ Song 2.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:gray;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-warning fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Exprimetere Te </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-warning">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Exprimere Te</h1>
+                <p class="lead">Express what you feel, let go your burdens, chill your mind, sing your lungs out!</p> 
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+                        echo "<h2 class='text-warning'>I Choose</h2>";
+                        echo "<p class='lead '>Alessia Cara</p>";
+
+                        echo "<p> [Instrumental Intro]</p>";
+                        echo "<h5><pre>C-F-Am-F</h5>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>C        </h5>";
+                        echo "<p> All of my life </p>";
+                        echo "<h5><pre>F             </h5>";
+                        echo "<p> I thought I was right </p>";
+                        echo "<h5><pre>Am               F </h5>";   
+                        echo "<p> Looking for something new </p>";
+                        echo "<h5><pre>C           </h5>";
+                        echo "<p> Stuck in my ways </p>";
+                        echo "<h5><pre>F        </h5>";
+                        echo "<p> Like old-fashioned days </p>";
+                        echo "<h5><pre>   Am               F </h5>";   
+                        echo "<p> But all the roads led me to you </p>";
+                        echo "<br/>";
+        
+                        echo "<h5><pre>Dm                   F      </h5>";
+                        echo "<p> The house that you live in don't make it a home </p>";
+                        echo "<h5><pre>     C                         G       </h5>";
+                        echo "<p> But feeling lonely don't mean you're alone </p>";
+                        echo "<h5><pre> Dm                  F  </h5>";
+                        echo "<p> People in life, they will come and they'll leave </p>";
+                        echo "<h5><pre> C         Am         G  </h5>";
+                        echo "<p> But if I had a choice I know where I would be </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>F                     C    </h5>";
+                        echo "<p> Through the lows and the highs I will stay by your side </p>";
+                        echo "<h5><pre>  G         Am       G             </h5>";
+                        echo "<p> There's no need for goodbyes, now I'm seeing the light </p>";
+                        echo "<h5><pre> F                 C   </h5>";
+                        echo "<p> When the sky turns to grey and there's nothing to say </p>";
+                        echo "<h5><pre>G                    Am </h5>";
+                        echo "<p> At the end of the day I choose you </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>Dm                      F    </h5>";
+                        echo "<p> The house that you live in don't make it a home </p>";    
+                        echo "<h5><pre>          C               G     </h5>";  
+                        echo "<p> But feeling lonely don't mean you're alone </p>";
+                        echo "<h5><pre>Dm              F       </h5>";
+                        echo "<p> I finally found where I feel I belong </p>";    
+                        echo "<h5><pre>C                    Am     G </h5>";  
+                        echo "<p> And I know you'll be there with wide open arms </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre> Am </h5>";
+                        echo "<p> I choose you</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>   F                 C     </h5>";
+                        echo "<p> Through the lows and the highs I will stay by your side </p>";
+                        echo "<h5><pre>G                    </h5>";
+                        echo "<p> There's no need for goodbyes, now I'm seeing the light </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>F                     C    </h5>";
+                        echo "<p> Through the lows and the highs I will stay by your side </p>";
+                        echo "<h5><pre>  G         Am       G             </h5>";
+                        echo "<p> There's no need for goodbyes, now I'm seeing the light </p>";
+                        echo "<h5><pre> F                 C   </h5>";
+                        echo "<p> When the sky turns to grey and there's nothing to say </p>";
+                        echo "<h5><pre>  G          Am G C</h5>";
+                        echo "<p> At the end of the day I choose you </p>";
+                        echo "<h5><pre>      AmG   C F Am</h5>";
+                        echo "<p> Oh, I choose you</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>Dm    Am C   </h5>";
+                        echo "<p> Oh, I choose you</p>";
+                        echo "<br/>";
+ 
+                       
+                        echo "-----------------------------------------";
+                        echo "<br/>";
+
+                        ?>
+                        Back to main page, Click <a href = "index.php"> here!
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Charlene Joy Layao</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
