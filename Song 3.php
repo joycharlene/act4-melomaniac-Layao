4e57b74fa679716c7f87dfc2e05c2f3acea0d66c
LayaoCharleneJoy <layaocharlenejoy@gmail.com>
2022-04-19 16:55:31 +0700

    Song 3 final

---
+++ Song 3.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:gray;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-warning fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Exprimetere Te </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-warning">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Exprimere Te</h1>
+                <p class="lead">Express what you feel, let go your burdens, chill your mind, sing your lungs out!</p> 
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+                        echo "<h2 class='text-warning'>Still Into You</h2>";
+                        echo "<p class='lead '>Paramore</p>";
+
+                        echo "<p> [Instrumental Intro]</p>";
+                        echo "<h5><pre>C-C-Am-G (2x)</h5>";
+                        echo "<br/>";
+                         
+                       
+
+                        echo "<h5><pre>C                        </h5>";
+                        echo "<p> Can't count the years on one hand </p>";
+                        echo "<h5><pre>C                 </h5>";
+                        echo "<p> That we've been together </p>";
+                        echo "<h5><pre>             Am</h5>";   
+                        echo "<p> I need the other one to hold you </p>";
+                        echo "<h5><pre>                   Am</h5>";
+                        echo "<p> Make you feel, make you feel better </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>C                 </h5>";
+                        echo "<p> It's not a walk in the park </p>";
+                        echo "<h5><pre>C             </h5>";
+                        echo "<p> To love each other </p>";
+                        echo "<h5><pre>                Am</h5>";
+                        echo "<p> But when our fingers interlock, </p>";
+                        echo "<h5><pre>                   Am</h5>";
+                        echo "<p> Can't deny, can't deny you're worth it </p>";
+                        echo "<h5><pre>                   F      G      C      </h5>";
+                        echo "<p> 'Cause after all this time I'm still into you </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>                   F      G   </h5>";
+                        echo "<p> I should be over all the butterflies </p>";
+                        echo "<h5><pre>          C            </h5>";
+                        echo "<p> But I'm into you (I'm into you) </p>";
+                        echo "<h5><pre>                   F      G   </h5>";
+                        echo "<p> And baby even on our worst nights </p>";
+                        echo "<h5><pre>        C            </h5>";
+                        echo "<p> I'm into you (I'm into you) </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>                   F        G </h5>";
+                        echo "<p> Let 'em wonder how we got this far </p>";    
+                        echo "<h5><pre>                     Am      C </h5>";  
+                        echo "<p> 'Cause I don't really need to wonder at all </p>";
+                        echo "<h5><pre>                   F      G      C      </h5>";
+                        echo "<p> Yeah, after all this time I'm still into you </p>";    
+                        echo "<br/>";
+
+                        echo "<h5><pre>C               </h5>";
+                        echo "<p> Recount the night that </p>";
+                        echo "<h5><pre>C               </h5>";
+                        echo "<p> I first met your mother </p>";
+                        echo "<h5><pre>                      Am </h5>";
+                        echo "<p> And on the drive back to my house </p>";
+                        echo "<h5><pre>                      Am </h5>";
+                        echo "<p> I told you that, I told you that I loved ya </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>C                     </h5>";
+                        echo "<p> You felt the weight of the world </p>";
+                        echo "<h5><pre>C               </h5>";
+                        echo "<p> Fall off your shoulder </p>";
+                        echo "<h5><pre>                    Am     </h5>";
+                        echo "<p> And to your favorite song </p>";
+                        echo "<h5><pre>                    Am  </h5>";
+                        echo "<p> We sang along to the start of forever </p>";
+                        echo "<h5><pre>                   F      G    C        </h5>";
+                        echo "<p> And after all this time I'm still into you </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>                   F      G   </h5>";
+                        echo "<p> I should be over all the butterflies </p>";
+                        echo "<h5><pre>          C            </h5>";
+                        echo "<p> But I'm into you (I'm into you) </p>";
+                        echo "<h5><pre>                   F      G   </h5>";
+                        echo "<p> And baby even on our worst nights </p>";
+                        echo "<h5><pre>C                  </h5>";
+                        echo "<p> I'm into you (I'm into you) </p>";
+                        echo "<h5><pre>                   F        G </h5>";
+                        echo "<p> Let 'em wonder how we got this far </p>";
+                        echo "<h5><pre>                     Am      C </h5>";
+                        echo "<p>'Cause I don't really need to wonder at all</p>";
+                        echo "<h5><pre>                   F      G      C      </h5>";
+                        echo "<p> Yeah, after all this time I'm still into you </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>C            C                    </h5>";
+                        echo "<p> Some things just, some things just make sense </p>";
+                        echo "<h5><prE>Am          G      </h5>";
+                        echo "<p> And one of those is you and I (Hey) </p>";
+                        echo "<h5><pre>C            C                    </h5>";
+                        echo "<p> Some things just, some things just make sense </p>";
+                        echo "<h5><pre>         Am      G            F G  </h5>";
+                        echo "<p> And even after all this time (Hey) </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>    C              F </h5>";
+                        echo "<p> I'm into you, baby, not a day goes by </p>";
+                        echo "<h5><pre>      G  C  </h5>";
+                        echo "<p> That I'm not into you </p>";
+                        echo "<br/>";
+ 
+                        echo "<h5><pre>                   F      G   </h5>";
+                        echo "<p> I should be over all the butterflies </p>";
+                        echo "<h5><pre>          C            </h5>";
+                        echo "<p> But I'm into you (I'm into you) </p>";
+                        echo "<h5><pre>                   F      G   </h5>";
+                        echo "<p> And baby even on our worst nights </p>";
+                        echo "<h5><pre>C                  </h5>";
+                        echo "<p> I'm into you (I'm into you) </p>";
+                        echo "<h5><pre>                   F        G </h5>";
+                        echo "<p> Let 'em wonder how we got this far </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>                     Am      C </h5>";
+                        echo "<p> 'Cause I don't really need to wonder at all </p>";
+                        echo "<h5><pre>              F </h5>";
+                        echo "<p> Yeah, after all this time </p>";
+                        echo "<h5><pre>   G      Am</h5>";
+                        echo "<p> I'm still into you </p>";
+                        echo "<h5><pre>   G      F</h5>";
+                        echo "<p> I'm still into you </p>";
+                        echo "<h5><pre>   G      C</h5>";
+                        echo "<p> I'm still into you </p>";
+                        echo "<br/>";
+
+                        echo "-----------------------------------------";
+                        echo "<br/>";
+
+                        ?>
+                        Back to main page, Click <a href = "index.php"> here!
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Charlene Joy Layao</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
