09aae23e6ad40adb2424bdc6b93d89b6c9345215
LayaoCharleneJoy <layaocharlenejoy@gmail.com>
2022-04-19 13:36:04 +0700

    Song 1 final

---
+++ Song 1.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:gray;">
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-warning fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Exprimetere Te </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-warning">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Exprimere Te</h1>
+                <p class="lead">Express what you feel, let go your burdens, chill your mind, sing your lungs out!</p> 
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php 
+                        echo "<h2 class='text-warning'>Enchanted</h2>";
+                        echo "<p class='lead '>Taylor Swift</p>";
+
+                        echo "<p> [Instrumental Intro]</p>";
+                        echo "<h5><pre>G-D-C-Em (2x)</h5>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>G               </h5>";
+                        echo "<p> There I was again tonight </p>";
+                        echo "<h5><pre>D                    </h5>";
+                        echo "<p> Forcing laughter, faking smiles </p>";
+                        echo "<h5><pre>C               G </h5>";   
+                        echo "<p> Same old tired lonely place </p>";
+                        echo "<br/>";
+        
+                        echo "<h5><pre>G             </h5>";
+                        echo "<p> Walls of insincerity, </p>";
+                        echo "<h5><pre>D                 </h5>";
+                        echo "<p> Shifting eyes and vacancy </p>";
+                        echo "<h5><pre> C                  G  </h5>";
+                        echo "<p> Vanished when I saw your face </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>                    C                 Em D  </h5>";  
+                        echo "<p> All I can say is it was enchanting to meet you </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>G                         </h5>";
+                        echo "<p>Your eyes whispered, Have we met? </p>";
+                        echo "<h5><pre>  D                       </h5>";
+                        echo "<p>Across the room your silhouette</p>";
+                        echo "<h5><pre> C                 G   </h5>";
+                        echo "<p>Starts to make its way to me</p>";
+                        echo "<h5><pre>G                     </h5>";
+                        echo "<p>The playful conversation starts</p>";
+                        echo "<h5><pre>D                     </h5>";
+                        echo "<p>Counter all your quick remarks</p>";
+                        echo "<h5><pre>C            G    </h5>";
+                        echo "<p>Like passing notes in secrecy</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>        C              Em     D</h5>";
+                        echo "<p>And it was enchanting to meet you</p>";    
+                        echo "<h5><pre>          C               Em     D</h5>";  
+                        echo "<p> All I can say is I was enchanted to meet you </p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre> C       G       D          C </h5>";
+                        echo "<p>This night is sparkling, don't you let it go</p>";
+                        echo "<h5><pre>            G        D        C   </h5>";
+                        echo "<p>I'm wonderstruck, blushing all the way home</p>";
+                        echo "<h5><pre>   C         G   D           C     </h5>";
+                        echo "<p>I'll spend forever wondering if you knew</p>";
+                        echo "<h5><pre>                   Em     D G</h5>";
+                        echo "<p>I was enchanted to meet you</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>G                        </h5>";
+                        echo "<p>The lingering question kept me up</p>";
+                        echo "<h5><pre>D                </h5>";
+                        echo "<p>2 AM, who do you love?</p>";
+                        echo "<h5><pre>C               G   </h5>";
+                        echo "<p>I wonder 'til I'm wide awake</p>";
+                        echo "<h5><pre>G                        </h5>";
+                        echo "<p>And now I'm pacing back and forth</p>";
+                        echo "<h5><pre>D                    </h5>";
+                        echo "<p>Wishing you were at my door</p>";
+                        echo "<h5><pre>C                   G     </h5>";
+                        echo "<p>I'd open up and you would say, Hey,</p>";
+                        echo "<h5><pre>  C              Em      D</h5>";
+                        echo "<p>It was enchanting to meet you,</p>";
+                        echo "<h5><pre>              C            Em      D </h5>";
+                        echo "<p>All I know is I was enchanted to meet you.</p>";
+                        echo "<br/>";
+ 
+                        echo "<h5><pre>      C        G       D          C       </h5>";
+                        echo "<p>This night is sparkling, don't you let it go</p>";
+                        echo "<h5><pre>            G        D        C   </h5>";
+                        echo "<p>I'm wonderstruck, blushing all the way home</p>";
+                        echo "<h5><pre>   C         G   D           C     </h5>";
+                        echo "<p>I'll spend forever wondering if you knew</p>";
+                        echo "<h5><pre>     C         G     D          C      </h5>";
+                        echo "<p>This night is flawless, don't you let it go</p>";
+                        echo "<h5><pre>          G         D           C</h5>";
+                        echo "<p>I'm wonderstruck, dancing around all alone</p>";
+                        echo "<h5><pre>   C         G   D           C     </h5>";
+                        echo "<p>I'll spend forever wondering if you knew</p>";
+                        echo "<h5><pre>          C        Em      D  G</h5>";
+                        echo "<p>I was enchanted to meet you</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>D                </h5>";
+                        echo "<p>This is me praying that</p>";
+                        echo "<h5><pre>C                  </h5>";
+                        echo "<p>This was the very first page</p>";
+                        echo "<h5><pre>  Em        G             </h5>";
+                        echo "<p>Not where the story line ends</p>";
+                        echo "<h5><pre>C                       </h5>";
+                        echo "<p>My thoughts will echo your name</p>";
+                        echo "<h5><pre> Em      D       </h5>";
+                        echo "<p>Until I see you again</p>";
+                        echo "<h5><pre>C                   </h5>";
+                        echo "<p>These are the words I held back</p>";
+                        echo "<h5><pre>   Em      D                </h5>";
+                        echo "<p>As I was leaving too soon</p>";
+                        echo "<h5><pre>  C             Em      D</h5>";
+                        echo "<p>I was enchanted to meet you</p>";
+                        echo "<br/>";
+  
+                        echo "<h5><pre>G                    D         </h5>";
+                        echo "<p>Please don't be in love with someone else</p>";
+                        echo "<h5><pre>C                    G          </h5>";
+                        echo "<p>Please don't have somebody waiting on you</p>";
+                        echo "<h5><pre>                        D           </h5>";
+                        echo "<p>Please don't be in love with someone else</p>";
+                        echo "<h5><pre>C                    G          </h5>";
+                        echo "<p>Please don't have somebody waiting on you</p>";
+                        echo "<br/>";
+
+                        echo "<h5><pre>      C        G       D          C       </h5>";
+                        echo "<p>This night is sparkling, don't you let it go</p>";
+                        echo "<h5><pre>            G        D        C   </h5>";
+                        echo "<p>I'm wonderstruck, blushing all the way home</p>";
+                        echo "<h5><pre>   C         G   D           C     </h5>";
+                        echo "<p>I'll spend forever wondering if you knew</p>";
+                        echo "<h5><pre>     C         G     D          C      </h5>";
+                        echo "<p>This night is flawless, don't you let it go</p>";
+                        echo "<p>(Please don't be in love with someone else)</p>";
+                        echo "<h5><pre>          G         D           C</h5>";
+                        echo "<p>I'm wonderstruck, dancing around all alone</p>";
+                        echo "<p>(Please don't have somebody waiting on you)</p>";
+                        echo "<h5><pre>   C         G   D           C     </h5>";
+                        echo "<p>I'll spend forever wondering if you knew</p>";
+                        echo "<p>(Please don't be in love with someone else)</p>";
+                        echo "<h5><pre>          C        Em      D  G</h5>";
+                        echo "<p>I was enchanted to meet you</p>";
+                        echo "<br/>"; 
+                         
+                        echo "<h5><pre>G                    D         </h5>";
+                        echo "<p>Please don't be in love with someone else</p>";
+                        echo "<h5><pre>C                    G          </h5>";
+                        echo "<p>Please don't have somebody waiting on you</p>";
+                        echo "<br/>";
+                        
+
+                        echo "-----------------------------------------";
+                        echo "<br/>";
+
+                        ?>
+                        Back to main page, Click <a href = "index.php"> here!
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Charlene Joy Layao</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
